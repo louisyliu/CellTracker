7717820ed83c5725220fc3680c3d2cba817769ed
Louis Lab <louisliu614@gmail.com>
2022-07-26 17:41:38 +0800

    fix: Fix blank output of trajmovie
    Output image range (i.e., size of time)  is incorrec.

--- trajmovie.m
+++ trajmovie.m
@@ -1,7 +1,7 @@
 function trajImg = trajmovie(traj, trajImg, varargin)
 %TRAJMOVIE creates trajectory movie.
-% trajImg = TRAJMOVIE(traj, img, trajNo) create the movie of trajectory of
-% index TrajNo. Plot all the trajectories by default.
+% trajImg = TRAJMOVIE(traj, img, trajNo, isExamineMode) create the movie of
+% trajectory of index TrajNo. Plot all the trajectories by default.
 
 trajNo = 1:length(traj);
 examineMode = false;
@@ -16,35 +16,14 @@ end
 
 trajTemp = traj(trajNo);
 trajFrame = cellfun(@(x) [x.Frame], trajTemp, 'UniformOutput',0)';
-frameDuration = cell2mat(cellfun(@(x) [x(1) x(end)], trajFrame, 'UniformOutput',0));
-time = min(frameDuration(:,1)):max(frameDuration(:,2));
+frameDuration = cell2mat(cellfun(@(x) [x(1); x(end)], trajFrame, 'UniformOutput',0));
+time = min(frameDuration(1,:)):max(frameDuration(2,:));
 
-% reshape(trajImg, [size(img,1:2) 1 size(img)])]
 trajImg = im2uint8((permute(repmat(trajImg(:,:,time), [1 1 1 3]),[1 2 4 3])));
 nTraj = length(trajNo);
 color = im2uint8(linspecer(nTraj));
 color = color(randperm(nTraj),:); % shuffle the color
 
-% for iTraj = 1:nTraj
-%     coord = cat(1, trajTemp{iTraj}.Centroid);
-%     frame = trajFrame{iTraj};
-% %     pixelFilled = findpixelbtpoints(coord);
-%     for t = 2:length(frame)
-%         pixelFilled = findpixelbtpoints(coord(1:t,:));
-%         for iPixel = 1:size(pixelFilled,1)
-%             trajImg(pixelFilled(iPixel,2),pixelFilled(iPixel,1),:,t) = color(iTraj,:);
-%             %             if examineMode
-%             %                 position = coord(t,:);
-%             %                 boxColor = color(iTraj,:);
-%             %                 textStr = num2str(trajNo(iTraj));
-%             %                 trajImg(:,:,:,t) = insertText(trajImg(:,:,:,t), position,textStr,'BoxColor',boxColor,TextColor='white',FontSize=24);
-%             %             end
-%         end
-%     end
-%     dispbar(iTraj, nTraj);
-% end
-
-% --------- updated algorithm
 for iTraj = 1:nTraj
     coord = cat(1, trajTemp{iTraj}.Centroid);
     frame = trajFrame{iTraj};
@@ -53,19 +32,10 @@ for iTraj = 1:nTraj
         pixelFilled = cell2mat(pixelFilledCell(1:t-1));
         for iPixel = 1:size(pixelFilled,1)
             trajImg(pixelFilled(iPixel,2),pixelFilled(iPixel,1),:,frame(t)) = color(iTraj,:);
-            %             if examineMode
-            %                 position = coord(t,:);
-            %                 boxColor = color(iTraj,:);
-            %                 textStr = num2str(trajNo(iTraj));
-            %                 trajImg(:,:,:,t) = insertText(trajImg(:,:,:,t), position,textStr,'BoxColor',boxColor,TextColor='white',FontSize=24);
-            %             end
         end
     end
     dispbar(iTraj, nTraj);
 end
-
-
-% ----------------------------
 
 
 if examineMode
@@ -85,8 +55,7 @@ if examineMode
             boxColor(iTraj,:) = color(trajOrder(iTraj),:);
             textStr{iTraj} = num2str(trajTNo(iTraj));
         end
-        trajImg(:,:,:,t) = insertText(trajImg(:,:,:,t),position,textStr,'BoxColor',boxColor,TextColor='white',FontSize=24 );
+        trajImg(:,:,:,t) = insertText(trajImg(:,:,:,t),position,textStr,'BoxColor',boxColor,TextColor='white', FontSize=24);
         dispbar(t, length(time));
     end
-
 end
