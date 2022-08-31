94180ed9037a2c0b138a4c1c782a899d602b04af
Louis Lab <louisliu614@gmail.com>
2022-08-31 23:48:44 +0800

    docs: Make a clear comment

--- demo.m
+++ demo.m
@@ -1,19 +1,20 @@
 %% Cell Tracker
 
 %% Load image
-filename = 'G:\exp_script\GitProject\sample\ofandsinglecelltracking_highdensity\20Xsinglecelltracking30ms20fps10s.nd2';
+filename = 'G:\fluorescentCells.nd2';
 img = nd2read(filename,1:100); % can't read tif for updated nd2sdkmatlab.
 
 %% Binarize image
-% use image binarizer
+% use image binarizer; load \sample\img.mat
 % export the image using 'Generate bw'
 bwgui(img);
 
-% or export the binarization parameters using 'Generate Para' and commented code below
+% or export the binarization parameters using 'Generate Para' and uncomment code below
 % for batch processing
 % bw = bwfun(@(x) getbw(x, bwPara), img);
 
-%% Track particles
+%% Track particles 
+% load \sample\bw.mat
 [traj] = trackbw(bw);
 trajFiltered = trajfilter(traj, 50);
 
