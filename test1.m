3e8c535f0dddcb63c450c8ce885379813d896113
Louis Lab <louisliu614@gmail.com>
2022-08-31 23:28:18 +0800

    refactor: Rename bw.mat to match the test file

--- test1.m
+++
@@ -1,9 +0,0 @@
-%trajLinkedMat
-
-for i = 1:size(trajLinkedMat, 1)
-    trajmat1 = logical(trajLinkedMat(i,:));
-    bwcomp = regionprops(trajmat1);
-    if length(bwcomp)>1
-        disp('')
-    end
-end
