0e90c2203067a63d090c634eed6baa089965d5be
Louis Lab <louisliu614@gmail.com>
2024-03-12 21:56:07 +0800

    fix: Resolve incorrect trajectory label when only one trajectory is present

--- tracking/linktraj.m
+++ tracking/linktraj.m
@@ -7,13 +7,21 @@ function trajMat = linktraj(matchedTraj)
 %   total time.   
 
 trajMat = matchedTraj{1};
+
+nTraj = length(matchedTraj)-1;
 for i = 1:length(matchedTraj)-1
-    start = trajMat(:,i+1);
+    start = trajMat(:,i+1);  
     M2 = matchedTraj{i+1};
+    if isempty(M2)
+        trajMat(:,i+2) = 0;
+        dispbar(i, nTraj);
+        continue;
+    end
     connect = M2(:,1);
     [LiEnd,LocStart] = ismember(start,connect);
     trajMat(:,i+2) = 0;
     trajMat(LiEnd, i+2) = M2(LocStart(LiEnd), 2);
     [LiEnd,~] = ismember(connect,start);
     trajMat(end+1:end+sum(~LiEnd), i+1:end) = M2(~LiEnd,:);
+    dispbar(i, nTraj);
 end
